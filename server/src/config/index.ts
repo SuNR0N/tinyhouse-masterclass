9791e308c73cb6996fa0908fe5af6e664e2a6c95
Norbert Annus <annus.norbert@gmail.com>
2020-04-04 17:36:39 +0100

    Replace REST endpoints with GraphQL

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,9 +1,11 @@
 interface Configuration {
+    API_PREFIX: string;
     ENVIRONMENT: string;
     PORT: number;
 }
 
 export const Configuration: Configuration = {
+    API_PREFIX: '/api',
     ENVIRONMENT: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT ? parseInt(process.env.PORT) : 9000,
 };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,14 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import { ApolloServer } from 'apollo-server-express';
 
+import { resolvers, typeDefs } from './graphql';
 import { Configuration } from './config';
-import { listings } from './listings';
 
-const { ENVIRONMENT, PORT } = Configuration;
+const { API_PREFIX, ENVIRONMENT, PORT } = Configuration;
 
 const app = express();
-
-app.use(bodyParser.json());
-
-// listings
-app.get('/listings', (_req, res) => {
-    return res.send(listings);
-});
-
-// delete listing
-app.delete('/listings/:id', (req, res) => {
-    const idToDelete: string = req.params.id;
-    const indexToDelete = listings.findIndex(({ id }) => id === idToDelete);
-    if (indexToDelete > -1) {
-        const [deletedListing] = listings.splice(indexToDelete, 1);
-        res.send(deletedListing);
-    } else {
-        res.status(404).end();
-    }
-});
+const server = new ApolloServer({ resolvers, typeDefs });
+server.applyMiddleware({ app, path: API_PREFIX });
 
 app.listen(PORT, () => {
     console.info(`Server is running at http://localhost:${PORT} in mode "${ENVIRONMENT}"`);
